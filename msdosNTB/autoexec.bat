5cbc9389c717a4c4d2d5bce052908fbd8591a2eb
Oramic <167904002+OramicGIT@users.noreply.github.com>
2025-02-11 19:43:08 +0200

    Add files via upload

---
+++ msdosNTB/autoexec.bat
@@ -0,0 +1,8 @@
+@echo off
+title MS-DOS Virtual Machine
+echo If you not copied NTVDM to System32, and you using Windows x64, close this app and copy NTVDM.
+pause  
+start /b sys\command.com
+cls
+echo Kernel:New Techology Bridge (NTB) 1.0
+echo Note:THIS IS NOT DOS, THIS IS JUST EMULATION!
---
+++ readme.md
@@ -0,0 +1,4 @@
+## 3.1NTB (NTB - New Techology Bridge)
+**3.1NTB** is a *project to make Windows 3.1* (only some programs) running in **NT**. This version includes:3.1NTB engine, MSDOS-NTB (to run MS-DOS console using NTVDM and command.com, and it can run some games. )
+
+
